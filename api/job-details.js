9ca18e5dc0af196da910aca481961f9fc30a1c78
Alondra Silva <alondra.silva@gmail.com>
2022-07-10 18:50:23 -0300

    job details modified

--- api/job-details.js
+++ api/job-details.js
@@ -5,47 +5,55 @@ var loadCardJobDetails = function () {
     fetch('https://62aa61db371180affbd48229.mockapi.io/jobsfinder/jobs/')
         .then(function (response) { return response.json(); })
         .then(function (data) {
+        var jobsDetails = data;
         var divTags = document.createElement('div');
         divTags.classList.add('d-flex');
         var divBtns = document.createElement('div');
         divBtns.classList.add('d-flex');
-        for (var id_1 in data) {
-            for (var prop in data[id_1]) {
-                if (data[id_1][prop] == params.get('id')) {
-                    for (var prop_1 in data[id_1]) {
-                        if (prop_1 == "title") {
-                            var h4 = document.createElement('h4');
-                            h4.textContent = "".concat(data[id_1][prop_1]);
-                            cardJobDetails.appendChild(h4);
-                        }
-                        else if (prop_1 == "description") {
-                            var p = document.createElement('p');
-                            p.textContent = "".concat(data[id_1][prop_1]);
-                            cardJobDetails.appendChild(p);
-                        }
-                        else if (prop_1 == 'location' || prop_1 == 'seniority' || prop_1 == 'category') {
-                            var p = document.createElement('p');
-                            var textP = document.createTextNode(data[id_1][prop_1]);
-                            p.classList.add('bg-info', 'card-tags-details');
-                            divTags.appendChild(p);
-                            p.appendChild(textP);
-                        }
-                    }
-                    var btnEdit = document.createElement('a');
-                    btnEdit.textContent = "Edit job";
-                    btnEdit.setAttribute('href', "./edit-job.html?id=".concat(data[id_1].id));
-                    btnEdit.classList.add('btn', 'btn-success', 'me-2');
-                    var btnDelete = document.createElement('a');
-                    btnDelete.textContent = "Delete job";
-                    btnDelete.classList.add('btn', 'btn-danger');
-                    btnDelete.setAttribute('href', "./delete-job.html?id=".concat(data[id_1].id));
-                    divBtns.appendChild(btnEdit);
-                    divBtns.appendChild(btnDelete);
-                }
+        jobsDetails.forEach(function (element) {
+            if (element.id === params.get('id')) {
+                // title
+                var h4 = document.createElement('h4');
+                var textH4 = document.createTextNode(element.title);
+                h4.appendChild(textH4);
+                cardJobDetails.appendChild(h4);
+                // description
+                var p = document.createElement('p');
+                var textP = document.createTextNode(element.description);
+                p.appendChild(textP);
+                cardJobDetails.appendChild(p);
+                // // location
+                var p_location = document.createElement('p');
+                var text_location = document.createTextNode(element.location);
+                p_location.classList.add('bg-info', 'card-tags');
+                p_location.appendChild(text_location);
+                divTags.appendChild(p_location);
+                // seniority
+                var p_seniority = document.createElement('p');
+                var text_seniority = document.createTextNode(element.seniority);
+                p_seniority.classList.add('bg-info', 'card-tags');
+                p_seniority.appendChild(text_seniority);
+                divTags.appendChild(p_seniority);
+                // category
+                var p_category = document.createElement('p');
+                var text_category = document.createTextNode(element.category);
+                p_category.classList.add('bg-info', 'card-tags');
+                p_category.appendChild(text_category);
+                divTags.appendChild(p_category);
+                cardJobDetails.appendChild(divTags);
+                var btnEdit = document.createElement('a');
+                btnEdit.textContent = "Edit job";
+                btnEdit.setAttribute('href', "./edit-job.html?id=".concat(data[id].id));
+                btnEdit.classList.add('btn', 'btn-success', 'me-2');
+                var btnDelete = document.createElement('a');
+                btnDelete.textContent = "Delete job";
+                btnDelete.classList.add('btn', 'btn-danger');
+                btnDelete.setAttribute('href', "./delete-job.html?id=".concat(data[id].id));
+                divBtns.appendChild(btnEdit);
+                divBtns.appendChild(btnDelete);
+                cardJobDetails.appendChild(divBtns);
             }
-            cardJobDetails.appendChild(divTags);
-            cardJobDetails.appendChild(divBtns);
-        }
+        });
     });
 };
 loadCardJobDetails();
--- api/job-details.ts
+++ api/job-details.ts
@@ -11,70 +11,76 @@ const loadCardJobDetails = () => {
     .then(response => response.json())
     .then(data => {
         
+        let jobsDetails = data
+
         const divTags = document.createElement('div')
         divTags.classList.add('d-flex')
 
         const divBtns = document.createElement('div')
         divBtns.classList.add('d-flex')
 
-        for(let id in data) {
+        jobsDetails.forEach(element => {
 
-            for(let prop in data[id]) {
+            if(element.id === params.get('id')) {
 
-                if(data[id][prop] == params.get('id')) {
+                // title
 
-                    for(let prop in data[id]) {
+                const h4 = document.createElement('h4')
+                const textH4 = document.createTextNode(element.title)
+                h4.appendChild(textH4)
+                cardJobDetails.appendChild(h4)
 
-                        if(prop == "title") {
+                // description
 
-                            const h4 = document.createElement('h4')
-                            h4.textContent = `${data[id][prop]}`
-    
-                            cardJobDetails.appendChild(h4)
+                const p = document.createElement('p')
+                const textP = document.createTextNode(element.description)
+                p.appendChild(textP)
 
-                        } else if(prop == "description") {
+                cardJobDetails.appendChild(p)
 
-                            const p = document.createElement('p')
-                            p.textContent = `${data[id][prop]}`
+                // // location
 
-                            cardJobDetails.appendChild(p)
-                        }else if(prop == 'location' || prop == 'seniority' || prop == 'category') {
+                const p_location = document.createElement('p')
+                const text_location = document.createTextNode(element.location)
+                p_location.classList.add('bg-info', 'card-tags')
+                p_location.appendChild(text_location)
+                divTags.appendChild(p_location)
 
-                            const p = document.createElement('p')
-                            const textP = document.createTextNode(data[id][prop])
-                            p.classList.add('bg-info', 'card-tags-details')
-    
-                           
-                            divTags.appendChild(p)
-                            p.appendChild(textP)
-                        }
-                        
-                    }      
-                    const btnEdit = document.createElement('a')
-                    btnEdit.textContent = "Edit job"
-                    btnEdit.setAttribute('href', `./edit-job.html?id=${data[id].id}`)
-                    btnEdit.classList.add('btn', 'btn-success', 'me-2')
-                       
-                    const btnDelete = document.createElement('a')
-                    btnDelete.textContent = "Delete job"
-                    btnDelete.classList.add('btn', 'btn-danger')
-                    btnDelete.setAttribute('href', `./delete-job.html?id=${data[id].id}`)
+                // seniority
 
-                    divBtns.appendChild(btnEdit)   
-                    divBtns.appendChild(btnDelete)
+                const p_seniority = document.createElement('p')
+                const text_seniority = document.createTextNode(element.seniority)
+                p_seniority.classList.add('bg-info', 'card-tags')
+                p_seniority.appendChild(text_seniority)
+                divTags.appendChild(p_seniority)
 
+                // category
 
-                }
+                const p_category = document.createElement('p')
+                const text_category = document.createTextNode(element.category)
+                p_category.classList.add('bg-info', 'card-tags')
+                p_category.appendChild(text_category)
+                divTags.appendChild(p_category)
+
+                cardJobDetails.appendChild(divTags)
+
+                const btnEdit = document.createElement('a')
+                btnEdit.textContent = "Edit job"
+                btnEdit.setAttribute('href', `./edit-job.html?id=${data[id].id}`)
+                btnEdit.classList.add('btn', 'btn-success', 'me-2')
+                    
+                const btnDelete = document.createElement('a')
+                btnDelete.textContent = "Delete job"
+                btnDelete.classList.add('btn', 'btn-danger')
+                btnDelete.setAttribute('href', `./delete-job.html?id=${data[id].id}`)
+
+                divBtns.appendChild(btnEdit)   
+                divBtns.appendChild(btnDelete)
+
+                cardJobDetails.appendChild(divBtns)
+
             }
-            cardJobDetails.appendChild(divTags)
-            cardJobDetails.appendChild(divBtns)
-            
-            
-            
-            
-
-        }
-
+        })
     })
 }
 
