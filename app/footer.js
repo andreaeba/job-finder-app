2b25610b30a644068d5cf46b6eadee7fe5657918
Andrea Bermúdez <80077290+andreaeba@users.noreply.github.com>
2022-10-05 22:10:30 -0300

    Update footer.js

--- app/footer.js
+++ app/footer.js
@@ -2,6 +2,6 @@ var body = document.body;
 var footer = document.createElement('footer');
 footer.classList.add('footer-container', 'bg-primary');
 var span = document.createElement('span');
-span.textContent = "Created by Andrea & Alondra";
+span.textContent = "Created by Andrea";
 body.appendChild(footer);
 footer.appendChild(span);
