db88f0956b504ab94bf196e2759f5e9529ad2398
Andrea Bermúdez <80077290+andreaeba@users.noreply.github.com>
2022-10-05 22:07:29 -0300

    Update footer.ts

--- app/footer.ts
+++ app/footer.ts
@@ -4,7 +4,7 @@ const footer = document.createElement('footer')
 footer.classList.add('footer-container','bg-primary')
 
 const span = document.createElement('span')
-span.textContent = "Created by Andrea & Alondra"
+span.textContent = "Created by Andrea"
 
 body.appendChild(footer)
 footer.appendChild(span)
